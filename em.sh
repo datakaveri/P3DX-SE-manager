15cf17ec36967341e2ccfc051af140e9452cfb7d
Kaushal <kaushal.kirpekar@datakaveri.org>
2023-08-17 12:38:33 +0530

    added virtual env

--- em.sh
+++ em.sh
@@ -3,8 +3,9 @@
 echo Starting Enclave Manager for Intel SGX
 
 #virtualenv env
-source activate
-#pip3 install -r requirements.txt
+source /home/iudx/.env/enclaveManager/bin/activate
+pip3 install -r requirements.txt
+
 export FLASK_APP=enclave-manager-server4.py
 export FLASK_ENV=development
 export FLASK_RUN_PORT=4000
